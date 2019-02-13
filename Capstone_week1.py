0d93ec1dd9a6361655960a52ce289e1fbc741777
olympique <47489539+olympique@users.noreply.github.com>
2019-02-13 12:17:21 -0800

    Update Capstone_week1.py

--- Capstone_week1.py
+++ Capstone_week1.py
@@ -1,23 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 #  This notebook will be used for the applied data science capstone project.
-
-# In[2]:
-
 
 import pandas as pd
 import numpy as np
 
-
-# In[3]:
-
-
 print('Hello Capstone Project Course!')
-
-
-# In[ ]:
-
-
-
-
